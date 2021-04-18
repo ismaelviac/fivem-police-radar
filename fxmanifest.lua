3522ac0203e8124a5c46f9d0a97f92fa073357e5
ismaelviac <59666577+ismaelviac@users.noreply.github.com>
2021-04-18 10:00:00 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,48 +1,14 @@
 fx_version 'cerulean'
 games { 'gta5' }
 
-description 'ESX Police Job'
+description 'ESX Radar'
 
-version '1.3.0'
+version '1.0'
 
-server_scripts {
-	'@mysql-async/lib/MySQL.lua',
-	'@es_extended/locale.lua',
-	'locales/br.lua',
-	'locales/de.lua',
-	'locales/en.lua',
-	'locales/pl.lua',
-	'locales/fr.lua',
-	'locales/fi.lua',
-	'locales/es.lua',
-	'locales/sv.lua',
-	'locales/ko.lua',
-	'locales/cs.lua',
-	'locales/nl.lua',
-	'config.lua',
-	'server/main.lua'
-}
+server_scripts {}
 
-client_scripts {
-	'@es_extended/locale.lua',
-	'locales/br.lua',
-	'locales/de.lua',
-	'locales/en.lua',
-	'locales/pl.lua',
-	'locales/fr.lua',
-	'locales/fi.lua',
-	'locales/es.lua',
-	'locales/sv.lua',
-	'locales/ko.lua',
-	'locales/cs.lua',
-	'locales/nl.lua',
-	'config.lua',
-	'client/main.lua',
-	'client/vehicle.lua',
-	'client/speed.lua'
-}
+client_scripts {'client/speed.lua'}
 
 dependencies {
-	'es_extended',
-	'esx_billing'
+	'esx_policejob'
 }
